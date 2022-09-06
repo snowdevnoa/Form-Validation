e35ae6193d42db18ceeb3e6c50d6bfd568ff2257
xmasnoaa <xmas.noaa@gmail.com>
2022-09-06 17:46:44 -0500

    name validation

---
+++ app.js
@@ -0,0 +1,23 @@
+//Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+  const name = document.getElementById('name');
+  console.log(name);
+  const re = /^[a-zA-Z]{2,10}$/;
+
+  if (!re.test(name.value)) {
+    name.classList.add('is-invalid');
+  } else {
+    name.classList.remove('is-invalid');
+  }
+}
+
+function validateZip() {}
+
+function validateEmail() {}
+
+function validatePhone() {}
